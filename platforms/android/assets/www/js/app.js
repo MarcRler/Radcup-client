3228bcbd70ef22cee86cd1c9762b99c63599c426
Pascal Weik <pascalweik@Pascals-MacBook-Pro-2.local>
2015-12-02 12:41:59 +0100

    Initial commit

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,67 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    .state('landing', {
+      url: '/',
+      templateUrl: 'templates/landing.html'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'templates/register.html'
+    })
+    .state('main', {
+      url: '/main',
+      // abstract: true,
+      templateUrl: 'templates/main.html'
+    })
+    .state('main.games', {
+      url: '/games',
+      views: {
+        'main-games': {
+          templateUrl: 'templates/main-games.html'
+        }
+      }
+    })
+    .state('main.settings', {
+      url: '/gameses',
+      views: {
+        'main-settings': {
+          templateUrl: 'templates/main-settings.html'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/');
+});
