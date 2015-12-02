07bb76fe4f552e8c7c733c1e3d817d8f8dbd47a4
Pascal Weik <pascalweik@Pascals-MacBook-Pro-2.local>
2015-12-02 15:30:49 +0100

    Add error handler to gamesController

--- www/js/gamesController.js
+++ www/js/gamesController.js
@@ -5,6 +5,8 @@ angular.module('radcup').controller('gamesController', function($scope, $http) {
     success(function(data) {
       $scope.games = data;
 
+    }).error(function(data,status){
+        console.log(data,status);
     });
   }
 );
