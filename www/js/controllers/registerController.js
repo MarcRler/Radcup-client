76ff8f1d2712531d1b9bccfa6ed705f24cdeaf3d
Immanuel Haag <Immi@Immanuels-MacBook-Pro.local>
2015-12-16 18:00:16 +0100

    new userService.js updated register and login Controller.

--- www/js/controllers/registerController.js
+++ www/js/controllers/registerController.js
@@ -1,49 +1,15 @@
-angular.module('radcup').controller('registerController', function($scope, $http, $httpParamSerializerJQLike, $location) {
+angular.module('radcup').controller('registerController', function($scope, userService, $location) {
+$scope.user={};
+$scope.register= function() {
+  userService.register($scope.user).then(function (data) {
+  console.log("register success in controllaaa");
+}, function (error) {
 
-          $scope.user = {};
-          $scope.submitForm = function() {
-          $http({
-            method  : 'POST',
-            url     : 'http://localhost:3000/api/users',
-          /*  data    : "username=" + encodeURIComponent($scope.user.username) +
-                     "&password=" + encodeURIComponent($scope.user.password) +
-                     "&email=" + encodeURIComponent($scope.user.email), funktioniert! */
-            data: $httpParamSerializerJQLike($scope.user),
-            headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-           })
-            .success(function(data) {
-              if (data.error) {
-                // Showing errors.
-                console.log(data.error.message);
+  console.log("register ERROR in controllaaa");
 
-                if (data.error.errmsg){
-                  $scope.user.email=data.error.errmsg;
 
-                }else {
-                  if (data.error.errors.email)
-                    $scope.user.email=data.error.errors.email.message;
+});
+};
 
-                  if (data.error.errors.username)
-                      $scope.user.username=data.error.errors.username.message;
 
-                  if (data.error.errors.password)
-                      $scope.user.password=data.error.errors.password.message;
-
-                }
-
-              } else {
-                console.log("Success creating: "+data.username);
-                $location.path("/register_succes");
-                var test = data;
-              }
-            });
-          };
-
-/* Use this for real authentication
-           ----------------------------------------------*/
-          //$http.post('/api/authenticate', { username: username, password: password })
-          //    .success(function (response) {
-          //        callback(response);
-          //    });
-}
-);
+});
---
+++ www/js/services/userService.js
@@ -0,0 +1,80 @@
+angular.module('radcup').service('userService', function($q, $http, $httpParamSerializerJQLike){
+  var host = 'http://localhost:3000/api/users/';
+
+ this.login =function(login){
+    console.log(login);
+    return $q(function(resolve, reject){
+      $http({
+          method: 'GET',
+          url: host + login.email,
+          headers:  {
+          'Authorization' : 'Basic '+ btoa(login.email+':'+login.password),
+          'Content-Type': 'application/x-www-form-urlencoded'
+          }
+          }).then(
+          function(data) {
+            console.log("erfolg");
+           if(login.email === data.data.email) {
+             this.persistData(data,login);
+              resolve(data);
+           }else {
+             alert("something strange");
+           }
+
+           },
+           function(error) {
+           console.log('error');
+           reject(error);
+        });
+     });
+
+
+    }
+
+  this.register =function(user){
+     return $q(function(resolve, reject){
+       $http({
+           method: 'POST',
+           url: host,
+           data:$httpParamSerializerJQLike(user),
+           headers:  {
+           'Content-Type': 'application/x-www-form-urlencoded'
+           }
+           }).then(
+           function(data) {
+             console.log("erfolg --> PROBLEM IM BACKEND immer 200 RES");
+             resolve(data);
+            },
+            function(error) {
+            console.log('error');
+            reject(error);
+         });
+      });
+     }
+
+  persistData = function(res,login){
+    console.log('persistData');
+    window.localStorage['email'] = login.email;
+    window.localStorage['password']=login.password;
+    window.localStorage['username']=res.data.username;
+    window.localStorage['userid']=res.data._id;
+
+  };
+
+  this.setUserHeader = function(){
+    var email=window.localStorage['email'];
+    var password=window.localStorage['password'];
+    $http.defaults.headers.common.Authorization = 'Basic '+ btoa(email+':'+password);
+    console.log('default header upon is: '+$http.defaults.headers.common.Authorization);
+    return true;
+  };
+
+  this.logout = function(){
+   delete $http.defaults.headers.common['Authorization'];
+   return true;
+  }
+
+
+   }
+
+);
