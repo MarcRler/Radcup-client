b0b442395715d0867b451a3ca2dfff30b8ee0ab8
Pascal Weik <pascalweik@Pascals-MacBook-Pro-3.local>
2015-12-16 16:34:12 +0100

    Game can be addet to database via service and Map

--- platforms/ios/www/js/mapController.js
+++ platforms/ios/www/js/mapController.js
@@ -1,6 +1,8 @@
 angular.module('radcup').controller('mapController', function($scope, locationService) {
 
   locationService.getPosition().then(function(position) {
+    var markers = [];
+
     if (!position) return;
     var map = new google.maps.Map(document.getElementById('map'), {
       center: position,
@@ -11,10 +13,38 @@ angular.module('radcup').controller('mapController', function($scope, locationSe
     var marker = new google.maps.Marker({
       position: position,
       map: map,
-      title: 'Your Position!'
+      title: 'Your Position!',
+      icon: 'img/location.png'
     });
 
-    
+    map.addListener('click', function(event) {
+      clearMarkers();
+      addMarker(event.latLng);
+    });
+
+    function addMarker(location) {
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map
+      });
+      markers.push(marker);
+    }
+
+    function setMapOnAll(map) {
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+      }
+    }
+
+    function clearMarkers() {
+      setMapOnAll(null);
+    }
+
+    function deleteMarkers() {
+      clearMarkers();
+      markers = [];
+    }
+
   });
 
 });
--- www/js/mapController.js
+++ www/js/mapController.js
@@ -1,6 +1,13 @@
-angular.module('radcup').controller('mapController', function($scope, locationService) {
+angular.module('radcup').controller('mapController', function($scope, locationService, $location, newGameService) {
+
+  var button = document.getElementById('asdf');
+  button.style.visibility = 'hidden';
+
+  var chosenPosition = null;
 
   locationService.getPosition().then(function(position) {
+    var markers = [];
+
     if (!position) return;
     var map = new google.maps.Map(document.getElementById('map'), {
       center: position,
@@ -11,10 +18,60 @@ angular.module('radcup').controller('mapController', function($scope, locationSe
     var marker = new google.maps.Marker({
       position: position,
       map: map,
-      title: 'Your Position!'
+      title: 'Your Position!',
+      icon: 'img/location.png'
     });
 
-    
+    map.addListener('click', function(event) {
+      clearMarkers();
+      button.style.visibility = 'visible';
+      chosenPosition = event.latLng;
+      console.log(chosenPosition);
+      addMarker(chosenPosition);
+    });
+
+    function addMarker(location) {
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        icon: 'img/destination.png'
+
+      });
+      markers.push(marker);
+    }
+
+    function setMapOnAll(map) {
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+      }
+    }
+
+    function clearMarkers() {
+      setMapOnAll(null);
+    }
+
+    function deleteMarkers() {
+      clearMarkers();
+      markers = [];
+    }
+
   });
+
+
+
+  $scope.createGame = function() {
+
+    newGameService.createNewGame(chosenPosition).then(function() {
+
+      alert("success");
+      $location.path("/main");
+
+    }, function(error) {
+
+      if (error == null) {
+        alert("fail")
+      }alert();
+    });
+  }
 
 });
