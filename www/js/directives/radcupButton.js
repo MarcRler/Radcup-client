49a80f6a22afdc04c088d2b6cccb45fe5adcd12d
mdotndot <m_neufeld@web.de>
2016-02-18 12:43:15 +0100

    fix radcup-button directive and add radcup-location directive

--- www/js/directives/radcupButton.js
+++ www/js/directives/radcupButton.js
@@ -2,6 +2,9 @@ angular.module('radcup').directive('radcupButton', function () {
   return {
     restrict: 'AE',
     transclude: true,
+    scope: {
+      outline: '='
+    },
     templateUrl: '/templates/partials/radcup-button.html'
-  };
+    }
 });
---
+++ www/js/directives/radcupLocation.js
@@ -0,0 +1,7 @@
+angular.module('radcup').directive('radcupLocation', function () {
+  return {
+    scope: true,
+    controller: 'gameOverviewController',
+    templateUrl: '/templates/partials/radcup-location.html'
+  };
+});
