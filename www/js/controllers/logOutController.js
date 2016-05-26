cf1ec15a1d231c211d8a2b1efeede1e0f4dff50a
Immanuel Haag <immae1@users.noreply.github.com>
2016-05-27 00:55:37 +0200

    bugfix with object object in login form after logout, ui-sref instead of h-ref refactoring

--- www/js/controllers/logOutController.js
+++ www/js/controllers/logOutController.js
@@ -1,11 +1,10 @@
 angular.module('radcup').controller('logOutController', function ($scope, userService, $location, $state, $window){
 /* logOutController dient als Bindeglied zwischen der settings view und
 der userService.logout function. */
-$scope.logout = {};
+
     $scope.logoutThisUser = function() {
       if(userService.logout()){
-          // wenn logout erfolgreich war, leite weiter auf login
-          $window.location = '#/landing';
+          $state.go('landing');
       } else{
         console.log('unknown error occured: ');
       }
