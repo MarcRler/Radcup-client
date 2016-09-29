92f9504c27c81d63e662643551571d8a3200ad8a
Immanuel Haag <immae1@users.noreply.github.com>
2016-09-29 21:29:37 +0200

    new colors in chart

--- www/js/controllers/statisticsController.js
+++ www/js/controllers/statisticsController.js
@@ -1,11 +1,16 @@
 angular.module('radcup').controller('statisticsController', function($scope, gamesService) {
 //http://krispo.github.io/angular-nvd3/#/quickstart
+var colors = ["#297239", "#EF473A", "#FF6F0E"];
+
   $scope.options = {
               chart: {
                   type: 'pieChart',
-                  height: 300,
+                  height: 500,
                   x: function(d){return d.key;},
                   y: function(d){return d.y;},
+                  color: function(d,i){
+                 return (d.data && d.data.color) || colors[i % colors.length]
+               },
                   showLabels: true,
                   duration: 500,
                   labelThreshold: 0.01,
@@ -24,21 +29,28 @@ angular.module('radcup').controller('statisticsController', function($scope, gam
           $scope.data = [
               {
                   key: "Won",
-                  y: 5
+                  y: 5,
+                  color:"#297239"
+
               },
               {
                   key: "Lost",
-                  y: 2
+                  y: 2,
+                  color:"#EF473A"
+
               },
               {
                   key: "Draw",
-                  y: 9
+                  y: 9,
+                  color:"#FF6F0E"
+
               }
           ];
 
     var statistic = gamesService.statistics().get();
 
     statistic.$promise.then(function(data) {
+    $scope.hide = true;
 
     $scope.won = data.won;
     $scope.lost = data.lost;
@@ -46,19 +58,21 @@ angular.module('radcup').controller('statisticsController', function($scope, gam
     $scope.data = [
         {
             key: "Won: "+data.won,
-            y: data.won
+            y: data.won,
+            color:"#297239"
         },
         {
             key: "Lost:"+data.lost,
-            y: data.lost
+            y: data.lost,
+            color:"#EF473A"
         },
         {
             key: "Draw: "+data.draw,
-            y: data.draw
+            y: data.draw,
+            color:"#FF6F0E"
         }
     ];
 
-        $scope.hide = true;
 
         $scope.won = data.won;
         $scope.lost = data.lost;
